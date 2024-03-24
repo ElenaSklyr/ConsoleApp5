a8469172436492f40e2e99e5aa7d8b2704dc9476
User <User@WIN-QS9VLSKO6R3>
2024-03-24 18:45:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,46 @@
+﻿//Тема 5. Урок 1. Константы и литералы
+/*Console.Write("Введите значение в миллиметрах:\n");
+double mm = double.Parse(Console.ReadLine());
+Console.WriteLine($"{mm*0.1} см");
+Console.WriteLine($"{mm * 0.001} м");
+Console.WriteLine($"{mm * 0.000001} м");
+Console.WriteLine($"{mm * 6.21371 * Math.Pow(10,-7)} миль");
+Console.WriteLine($"{mm /914.4} ярд");
+
+using System;
+
+Console.WriteLine("--------------------------------------------\n" +
+    "********************************************");
+Console.WriteLine($"{"N",-4}{"Название",-20}{"Цена",-6}{"Кол-во",-8}{"Сумма",-6}");
+Console.WriteLine($"{"1",-4}{"КОФЕ СТАНДАРТНЫЙ",-20}{"69.00",-6}{"1.00",-8}{"69.00",-6}");
+Console.WriteLine($"{" ",-4}{"НДС с расчитанной",-20}{" ",-6}{" ",-8}{"10.53",-6}");
+Console.WriteLine($"{" ",-4}{"ставкой 18%",-20}{" ",-6}{" ",-8}{" ",-6}");
+Console.WriteLine(" ");
+Console.WriteLine($"{"2",-4}{"РУЛЕТ-СПРИНГ ТВ ИЗ",-20}{"70.00",-6}{"1.00",-8}{"70.00",-6}");
+Console.WriteLine($"{" ",-4}{"КУ",-20}{" ",-6}{" ",-8}{" ",-6}");
+Console.WriteLine($"{" ",-4}{"НДС с расчитанной",-20}{" ",-6}{" ",-8}{"6.36",-6}");
+Console.WriteLine($"{" ",-4}{"ставкой 10%",-20}{" ",-6}{" ",-8}{" ",-6}");
+Console.WriteLine(" \n" +
+    "\n");
+Console.WriteLine("********************************************");
+Console.WriteLine($"{"ИТОГО:",-37}{"139.00"}");
+Console.WriteLine(" \n" +
+    "\n");
+Console.WriteLine($"{"Электронные средства",-37}{"139.00"}");
+Console.WriteLine($"{"НДС итога чека с",-37}{"10.53"}");
+Console.WriteLine($"{"расчитанной ставкой"}");
+Console.WriteLine($"{"18%"}");
+Console.WriteLine($"{"НДС итога чека с",-37}{"6.36"}");
+Console.WriteLine($"{"расчитанной ставкой"}");
+Console.WriteLine($"{"10%"}");
+Console.WriteLine(" \n");
+Console.WriteLine("********************************************");
+Console.WriteLine($"{"ВИД НАЛОГООБЛОЖЕНИЯ:"}{" ОСН"}");
+Console.WriteLine($"{"РЕГ. НОМЕР ККТ:"}{" 000143873023483"}");
+Console.WriteLine($"{"ЗАВОД №:"}");
+Console.WriteLine($"{"ФН №:"}{" 8710000100265168"}");
+Console.WriteLine($"{"ФД №:"}{" 6026"}");
+Console.WriteLine($"{"ФПД:"}{" 3322278314"}");*/
+
+
+
